96c3462f27dd1ca802c64aad0388ce586d8dcc0d
bbhan3 <bbhan3@iflytek.com>
2020-04-21 22:27:23 +0800

    byh

---
+++ byh_web/src/main/java/com/aiit/byh/byh_web/ByhWebApplication.java
@@ -0,0 +1,13 @@
+package com.aiit.byh.byh_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByhWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByhWebApplication.class, args);
+    }
+
+}
